ee6a73d7087d7f785659a531135f36abb5f9b0a2
Bharath Mohan <broxdeez@Bharaths-MBP.attlocal.net>
2024-08-10 20:01:35 -0700

    init

---
+++ portfolio/portfolio.py
@@ -0,0 +1,28 @@
+"""Welcome to Reflex! This file outlines the steps to create a basic app."""
+
+import reflex as rx
+
+from rxconfig import config
+
+
+class State(rx.State):
+    """The app state."""
+
+
+@rx.page(route="/", title="Landing")
+def index() -> rx.Component:
+    # Welcome Page (Index)
+    return rx.vstack(rx.heading("HELLO!!"))
+
+
+style = {
+    "font_family": "Monaspace Neon",
+    "font_size": "16px",
+}
+
+app = rx.App(
+    style=style,
+    theme=rx.theme(
+        appearance="light", has_background=True, radius="large", accent_color="teal"
+    ),
+)
